0e4e812ab21e1d6174493484f9c3d24598c4afa3
rockyhappy <rachitkatiyar869@gmail.com>
2023-10-23 23:21:40 +0530

    Splash Screen: Status Bar Fixed

---
+++ app/src/main/java/com/example/myapplication/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.caverock.androidsvg.SVG
+import com.caverock.androidsvg.SVGImageView
+
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        val svgImageView = findViewById<SVGImageView>(R.id.svgImageView)
+        val svg = SVG.getFromResource(resources, R.raw.b)
+        svgImageView.setSVG(svg)
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val intent= Intent(this,Login::class.java)
+                startActivity(intent)
+                finish()
+            },4000
+        )
+    }
+}
