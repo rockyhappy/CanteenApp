4086b16a801177821944e35937f455e172658555
rockyhappy <rachitkatiyar869@gmail.com>
2023-10-24 01:40:11 +0530

    Login Screen: Fragments Structure Added

---
+++ app/src/main/java/com/example/myapplication/fragments/LoginFragment.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.myapplication.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        val button = view.findViewById<Button>(R.id.btn)
+
+        button.setOnClickListener {
+            // Replace FragmentA with FragmentB
+            val fragmentTransaction = parentFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.flFragment, ForgotPassward())
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
+        return view
+    }
+}
