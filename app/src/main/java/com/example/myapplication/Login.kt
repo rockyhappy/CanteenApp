c2975f24e25a4d6cc793919f03077d37a6c593a5
rockyhappy <rachitkatiyar869@gmail.com>
2023-10-23 23:57:37 +0530

    Registration Screen: Initial Files

--- app/src/main/java/com/example/myapplication/Login.kt
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -2,10 +2,15 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
+        binding=ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
