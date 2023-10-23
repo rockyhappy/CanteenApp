8cbe5e5740d20e1ac95db0a64a5ba84a3f22d1e3
rockyhappy <rachitkatiyar869@gmail.com>
2023-10-24 04:06:10 +0530

    Login Screen: Login Fragment Layout completed

--- app/src/main/java/com/example/myapplication/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import com.example.myapplication.R
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
@@ -15,15 +16,14 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_login, container, false)
-//        val button = view.findViewById<Button>(R.id.btn)
-//
-//        button.setOnClickListener {
-//            // Replace FragmentA with FragmentB
-//            val fragmentTransaction = parentFragmentManager.beginTransaction()
-//            fragmentTransaction.replace(R.id.flFragment, ForgotPassward())
-//            fragmentTransaction.addToBackStack(null)
-//            fragmentTransaction.commit()
-//        }
+        val forgotPassword = view.findViewById<TextView>(R.id.forgot_pass)
+
+        forgotPassword.setOnClickListener {
+            val fragmentTransaction = parentFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.flFragment, ForgotPassward())
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
 
         return view
     }
