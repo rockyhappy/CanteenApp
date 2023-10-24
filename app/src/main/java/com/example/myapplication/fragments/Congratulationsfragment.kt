2c82326f07ce2eb7dfa27c707932b575b2bdd73f
rockyhappy <rachitkatiyar869@gmail.com>
2023-10-25 01:59:04 +0530

    Registration: Nameing Corrected

--- app/src/main/java/com/example/myapplication/fragments/Congratulationsfragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/Congratulationsfragment.kt
@@ -1,20 +1,22 @@
-package com.example.myapplication
+package com.example.myapplication.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
 
-class registrationfragment : Fragment(R.layout.fragment_registrationfragment) {
-
+class congratulationsfragment : Fragment(R.layout.fragment_congratulationsfragment) {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view= inflater.inflate(R.layout.fragment_registrationfragment, container, false)
+        val view= inflater.inflate(R.layout.fragment_congratulationsfragment, container, false)
+
+
 
 
         return view
---
+++ app/src/main/java/com/example/myapplication/fragments/Registrationfragment.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+
+class Registrationfragment : Fragment(R.layout.fragment_registrationfragment) {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view= inflater.inflate(R.layout.fragment_registrationfragment, container, false)
+
+
+        return view
+    }
+
+}
