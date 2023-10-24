a52b4b1eb6d4390ee0b780262cb7b31c84365343
rockyhappy <rachitkatiyar869@gmail.com>
2023-10-25 02:25:23 +0530

    Registration: Completed Registration Fragment

--- app/src/main/java/com/example/myapplication/fragments/Registrationfragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/Registrationfragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.caverock.androidsvg.SVG
+import com.caverock.androidsvg.SVGImageView
 import com.example.myapplication.R
 
 class Registrationfragment : Fragment(R.layout.fragment_registrationfragment) {
@@ -17,7 +19,15 @@ class Registrationfragment : Fragment(R.layout.fragment_registrationfragment) {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_registrationfragment, container, false)
 
-
+        val svgImageView = view.findViewById<SVGImageView>(R.id.svgImageView)
+        var svg = SVG.getFromResource(resources, R.raw.backbutton)
+        svgImageView.setSVG(svg)
+        svgImageView.setOnClickListener{
+            val fragmentTransaction = parentFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.flFragment, ChoiceFragment())
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
         return view
     }
 
