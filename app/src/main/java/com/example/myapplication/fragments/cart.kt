a6b1738b6c23ea2726f53fe73795e0976bdb6e6a
rockyhappy <115190222+rockyhappy@users.noreply.github.com>
2023-11-13 05:16:02 +0530

    DishCategory: Button Clicks Added

--- app/src/main/java/com/example/myapplication/fragments/cart.kt
+++ app/src/main/java/com/example/myapplication/fragments/cart.kt
@@ -1,16 +1,27 @@
 package com.example.myapplication.fragments
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.createDataStore
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import com.example.myapplication.RetrofitInstance2
+import com.example.myapplication.RvAdapter2
 import kotlinx.coroutines.launch
 
 class cart : Fragment() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var rvadapter : RvAdapter2
+    private lateinit var dataStore: DataStore<Preferences>
+    private var dialog: Dialog? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,10 +29,10 @@ class cart : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_cart, container, false)
-
+        dataStore = requireContext().createDataStore(name = "user")
         lifecycleScope.launch{
             try{
-
+//                val response = RetrofitInstance2.getApiServiceWithToken(dataStore = )
             }catch (e: Exception)
             {
 
