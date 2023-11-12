b4e050fa9a0f4d5d92167b72e33fd42a14d0ec00
rockyhappy <115190222+rockyhappy@users.noreply.github.com>
2023-11-13 04:32:27 +0530

    RecyclerAdapter: Cart Adapter Added

--- app/src/main/java/com/example/myapplication/fragments/cart.kt
+++ app/src/main/java/com/example/myapplication/fragments/cart.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.example.myapplication.R
+import kotlinx.coroutines.launch
 
 class cart : Fragment() {
 
@@ -17,6 +19,16 @@ class cart : Fragment() {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_cart, container, false)
 
+        lifecycleScope.launch{
+            try{
+
+            }catch (e: Exception)
+            {
+
+            }finally{
+
+            }
+        }
         return view;
     }
 
