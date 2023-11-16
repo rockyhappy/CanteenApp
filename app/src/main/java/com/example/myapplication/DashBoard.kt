0976973d933452eb2c1505b66d5ab93e8079d3f0
rockyhappy <115190222+rockyhappy@users.noreply.github.com>
2023-11-16 18:48:40 +0530

    Canteen Menu: Upadated Layout

--- app/src/main/java/com/example/myapplication/DashBoard.kt
+++ app/src/main/java/com/example/myapplication/DashBoard.kt
@@ -190,6 +190,13 @@ class DashBoard : AppCompatActivity() {
                 }
                 2 -> {
                     // Handle the "Search" tab selection
+
+                    supportFragmentManager.popBackStack()
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.flFragment, Dishes_Category())
+                        commit()
+
+                    }
                 }
                 3 -> {
                     // Handle the "Profile" tab selection
