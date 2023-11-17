07620d4022ffa04fffc9c4d826fe3f3c861d0c83
Deepika <dubeydeepika1209@gmail.com>
2023-11-17 17:28:46 +0530

    Help & Support Page : Added spinner

--- app/src/main/java/com/example/myapplication/fragments/HelpSupport.kt
+++ app/src/main/java/com/example/myapplication/fragments/HelpSupport.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 
@@ -22,6 +25,8 @@ class HelpSupport : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var optionsSpinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -35,8 +40,36 @@ class HelpSupport : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help_support, container, false)
+        val view = inflater.inflate(R.layout.fragment_help_support, container, false)
+
+
+        optionsSpinner = view.findViewById(R.id.optionsSpinner)
+
+        // Set up the options for the Spinner
+        val options = arrayOf("Order Related Complain", "Order Related Ingredients", "Order Related Menu","Service Related")
+        val adapter = ArrayAdapter(requireContext(), R.layout.fragment_help_support, options)
+        adapter.setDropDownViewResource(R.layout.fragment_help_support)
+
+        // adapter for the Spinner
+        optionsSpinner.adapter = adapter
+
+        // listener to handle item selection
+        optionsSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parentView: AdapterView<*>?, selectedItemView: View?, position: Int, id: Long) {
+                // Handle the selected item
+                val selectedOption = options[position]
+                // You can do something with the selected option
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>?) {
+                // Do nothing here
+            }
+        }
+
+        return view
     }
+
+
 
     companion object {
         /**
